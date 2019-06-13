37f86233949cdfb8704833e0b7febd0c52c3e17c
ramtyt <51030729+ramtyt@users.noreply.github.com>
2019-06-13 23:16:52 +0530

    Update ballBlast4.js

--- ballBlast4.js
+++ ballBlast4.js
@@ -1,29 +1,28 @@
-var canvas=document.getElementById("canvas");
-var cxt=canvas.getContext("2d");
+var canvas=document.getElementById("canvas"); //creating some space to draw
+var cxt=canvas.getContext("2d");              //Getting access to the tool
 
 var width=600;
 var height=600;
+                                               //Declaring variables for the canon
 var canon_x=width/2-25;
 var canon_y=height-50;
 var canon_w=50;
 var canon_h=50;
+
 var i;
 var score=0;
 
-
+                                            //Declaring variables for event listeners
 var leftPressed=false;
 var rightPressed=false;
 var enterPressed=false;
-
-var enemyTotal=100;
-var enemies=[];
-var enemy_x;
-var enemy_y;
-var enemyRadius;
+                                              
 
 
 
 
+
+                                              //Declaring variables,array for bullet
 var bulletTotal=1;
 var bullets=[];
 var bullet_x;
@@ -33,7 +32,7 @@ var bulletRadius=5;
 
 document.addEventListener("keydown",keyDown,false);
 document.addEventListener("keyup",keyUp,false);
-
+                                                      //Defining a function for keydown event
 function keyDown(e){
 	if(e.key=="ArrowLeft")
 	{
@@ -45,12 +44,12 @@ function keyDown(e){
 	}
 	if(e.key=="Enter" && bullets.length<bulletTotal)
 	{
-		bullets.push([canon_x+25,canon_y])
+		bullets.push([canon_x+25,canon_y])          //pushing array into another array named bullets
 	}
 
 }
 
-
+                                                          //Defining a function for keyup event
 function keyUp(e){
 	if(e.key=="ArrowLeft")
 	{
@@ -63,23 +62,23 @@ function keyUp(e){
 	
 }
 
-
+                                                          //Defining a constructor function for enemies
 function Enemie(enemy_x,enemy_y,dx,dy,status){
 	this.enemy_x=enemy_x;
 	this.enemy_y=enemy_y;
 	this.dx=dx;
 	this.dy=dy;
 	this.status=status;
-	this.draw=function(){
+	this.draw=function(){                                //To draw out the enemies
 		
           cxt.beginPath();
           cxt.arc(this.enemy_x,this.enemy_y,50,0,Math.PI*2);
-          cxt.fillStyle="red";
+          cxt.fillStyle="yellow";
           cxt.fill();
           cxt.closePath();
 
 	}
-	this.moveEnemy=function(){
+	this.moveEnemy=function(){                             //To move the enemies around
 		
         this.enemy_x+=this.dx;
         this.enemy_y+=this.dy;
@@ -106,7 +105,7 @@ function Enemie(enemy_x,enemy_y,dx,dy,status){
 
 }
 	
-var enemies=[];
+var enemies=[];                      //Declaring variables,array for enemies
 var dx=7;
 var dy=4;
 var enemy_x=50;
@@ -116,7 +115,7 @@ for(i=0;i<100;i++)
 {   
     
     
-	enemies.push(new Enemie(enemy_x,enemy_y,dx,dy))
+	enemies.push(new Enemie(enemy_x,enemy_y,dx,dy))    //pushing an object Enemie into array named enemies
 	enemy_x-=400;
 
 
@@ -124,7 +123,7 @@ for(i=0;i<100;i++)
 }	
 
 
-function drawBullet(){
+function drawBullet(){                             //Defining function to draw bullet
 	for(i=0;i<bullets.length;i++)
 	{
 		cxt.beginPath();
@@ -135,18 +134,18 @@ function drawBullet(){
 	}
 }
 
-function fireBullet(){
+function fireBullet(){                              //Defining function to fire the bullet up
 	for(i=0;i<bullets.length;i++)
 	{
 		bullets[i][1]-=30;
 		if(bullets[i][1]<0)
 		{
-			bullets.splice(i,1)
+			bullets.splice(i,1)                   //removing the bullet from array after some certain conditions
 		}
 	}
 }
 
-function collideTest(){
+function collideTest(){                          //function to check collision b/w bullet and enemy
     
 	for(i=0;i<bullets.length;i++)
 	{
@@ -154,12 +153,12 @@ function collideTest(){
 			
 			if(bullets[i][0]>=enemies[j].enemy_x&&bullets[i][0]<=enemies[j].enemy_x+50&&bullets[i][1]>=enemies[j].enemy_y&&bullets[i][1]<=enemies[j].enemy_y+50)
 			{   
-				score++;
+				score++;                          //countiong the score
 			
-				bullets.splice(i,1);
+				bullets.splice(i,1);              //making the bullet disappear after hit
 			
 			
-				enemies.splice(j,1);
+				enemies.splice(j,1);             //making the enemy disappear after hit
                 
 			}
 			
@@ -168,14 +167,14 @@ function collideTest(){
 }
 
 
-function collideCanon(){
+function collideCanon(){                        //function to check collision b/w canon and enemy
 	for(var i=0;i<100;i++)
 	{
 		if(enemies[i].enemy_x+50>canon_x&&enemies[i].enemy_x+50<canon_x+50&&enemies[i].enemy_y+50>canon_y&&enemies[i].enemy_y+50<canon_y+50)
 			{
 			alert("GAME OVER -->YOU LOSE");
-			document.location.reload();
-		    clearInterval(interval);
+			document.location.reload();        //reloads the document
+		    clearInterval(interval);           //clears out  the interval
 			}
 		
   
@@ -188,14 +187,14 @@ function collideCanon(){
 
 
 
-function drawCanon(){
+function drawCanon(){                                  //function to draw canon
     cxt.beginPath();
 	cxt.rect(canon_x,canon_y,canon_w,canon_h);
 	cxt.fillStyle="red";
 	cxt.fill();
 	cxt.closePath();
 
-	if(rightPressed && canon_x<canvas.width-canon_w)
+	if(rightPressed && canon_x<canvas.width-canon_w)    //conditions for moving out the canon
 	{
        canon_x+=5;
        for(i=0;i<bullets.length;i++){
@@ -210,7 +209,7 @@ function drawCanon(){
        }
 	}
 }
-function scoreTotal(){
+function scoreTotal(){                        //function to display out total score
 	
 	cxt.font="30px Arial";
 	cxt.fillText("score:"+score,450,50);
@@ -219,7 +218,7 @@ function scoreTotal(){
 }
 
 
-function game(){
+function game(){                                    //function where all other functions are invoked just for presentation
 	cxt.clearRect(0,0,canvas.width,canvas.height);
 	drawCanon();
 	for(i=0;i<enemies.length;i++){
@@ -236,4 +235,4 @@ function game(){
 		
 }
 
-var interval=setInterval(game,25);
+var interval=setInterval(game,25);         //The function game is called every 25 milli seconds
